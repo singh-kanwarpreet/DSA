14b7184fb2e193e27f44c800817efbd26fc366a2
Kanwarpreet Singh <kanwarpreet388@gmail.com>
2025-05-21 14:37:35 +0530

    Create recover bst.java

---
+++ mixed question 1/recover bst.java
@@ -0,0 +1,21 @@
+
+class Solution {
+    TreeNode first = null,second = null, prev = new TreeNode(Integer.MIN_VALUE);
+    
+    public void recoverTree(TreeNode root) {
+        inorder(root);
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+    public void inorder(TreeNode root){
+        if(root == null) return;
+        inorder(root.left);
+        if(prev.val > root.val){
+            if(first == null) first = prev;
+            second = root;
+        }
+        prev = root;
+        inorder(root.right);
+    }
+}
